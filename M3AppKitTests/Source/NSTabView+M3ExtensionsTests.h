ce69d146aa5d31645a4f6b1c93152d173584acae
Martin Pilkington <pilky@mcubedsw.com>
2013-02-11 15:49:28 +0000

    Finished NSTabView+M3Extensions

---
+++ M3AppKitTests/Source/NSTabView+M3ExtensionsTests.h
@@ -0,0 +1,13 @@
+//
+//  NSTabView+M3ExtensionsTests.h
+//  M3AppKit
+//
+//  Created by Martin Pilkington on 11/02/2013.
+//  Copyright (c) 2013 M Cubed Software. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NSTabView_M3ExtensionsTests : SenTestCase
+
+@end
---
+++ M3AppKitTests/Source/NSTabView+M3ExtensionsTests.m
@@ -0,0 +1,32 @@
+//
+//  NSTabView+M3ExtensionsTests.m
+//  M3AppKit
+//
+//  Created by Martin Pilkington on 11/02/2013.
+//  Copyright (c) 2013 M Cubed Software. All rights reserved.
+//
+
+#import "NSTabView+M3ExtensionsTests.h"
+#import <M3AppKit/M3AppKit.h>
+
+@implementation NSTabView_M3ExtensionsTests
+
+- (void)test_returnsIndexOfSelectedTab {
+	NSTabView *tabView = [NSTabView new];
+	
+	NSTabViewItem *item1 = [[NSTabViewItem alloc] initWithIdentifier:@"1"];
+	NSTabViewItem *item2 = [[NSTabViewItem alloc] initWithIdentifier:@"2"];
+	NSTabViewItem *item3 = [[NSTabViewItem alloc] initWithIdentifier:@"3"];
+	
+	[tabView addTabViewItem:item1];
+	[tabView addTabViewItem:item2];
+	[tabView addTabViewItem:item3];
+	
+	assertThatInteger(tabView.m3_indexOfSelectedTab, is(equalToInteger(0)));
+	
+	[tabView selectTabViewItemAtIndex:1];
+	
+	assertThatInteger(tabView.m3_indexOfSelectedTab, is(equalToInteger(1)));
+}
+
+@end
